eaef3479e192fa77a8c6c57bb145c15404d43e61
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2020-04-21 17:59:19 -0400

    Step 1-4 complete: index.js

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,22 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const cardsL = document.querySelector('.cards');
+
+axios.get("https://api.github.com/users/Rezz888")
+
+.then((response)=>{
+  console.log(response);
+  
+  cardsL.appendChild(componentFunction(response.data))
+
+})
+.catch((err)=>{
+  
+})
+
+// console.log(axios);
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +40,13 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  tetondan,
+  dustinmyers,
+  justsml,
+  luishrd,
+  bigknell
+];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +67,58 @@ const followersArray = [];
 </div>
 
 */
+function componentFunction(dataP){
+
+const cardContainer = document.createElement('div');
+cardContainer.classList.add('card');
+
+const image = document.createElement('img');
+image.src = dataP.avatar_url;
+cardContainer.appendChild(image);
+
+const cardInfo = document.createElement('div');
+cardInfo.classList.add('card-info');
+cardContainer.appendChild(cardInfo);
+
+let userName = document.createElement('h3');
+userName.classList.add('name');
+userName.textContent = dataP.name;
+cardInfo.appendChild(userName);
+
+const p1 = document.createElement('p');
+p1.classList.add('username');
+p1.textContent = dataP.login;
+cardInfo.appendChild(p1);
+
+const p2 = document.createElement('p'); 
+p2.textContent = dataP.location;
+cardInfo.appendChild(p2);
+
+const p3 = document.createElement('p');
+p3.textContent = 'Profile: ';
+cardInfo.appendChild(p3);
+
+
+const anchorT = document.createElement('a');
+anchorT.href = dataP.html_url;
+anchorT.textContent = dataP.html_url;
+p3.appendChild(anchorT);
+
+const p4 = document.createElement('p');
+p4.textContent = `Followers: ${dataP.followers}`;
+cardInfo.appendChild(p4);
+
+const p5 = document.createElement('p');
+p5.textContent = `Following: ${dataP.following}`;
+cardInfo.appendChild(p5);
+
+const p6 = document.createElement('p');
+p6.textContent = `Bio: ${dataP.bio}`;
+cardInfo.appendChild(p6);
+
+return cardContainer;
+}
+
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -54,8 +128,3 @@ const followersArray = [];
   bigknell
 */
 
-console.log(axios.get("https://api.github.com/users/Rezz888"))
-// .then()
-// .catch
-
-// console.log(axios);
